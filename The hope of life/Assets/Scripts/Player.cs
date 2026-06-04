1ab122ac04c0d6eb85124e4460f9bc142a7f8020
Italojavosky11 <silva.camilo@escolar.ifrn.edu.br>
2026-06-04 12:06:41 -0300

    show
    show

--- The hope of life/Assets/Scripts/Player.cs
+++ The hope of life/Assets/Scripts/Player.cs
@@ -14,11 +14,18 @@ public class Player : MonoBehaviour
         
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        Flip();
     }
 
     void FixedUpdate()
     {
         
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void Flip() {
+        if(movement.x > 0) {transform.eulerAngles = new Vector2(0, 0);}
+        else if(movement.x < 0) {transform.eulerAngles = new Vector2(0, 180);}
     }
 }
