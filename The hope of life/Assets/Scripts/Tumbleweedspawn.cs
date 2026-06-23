a4956f04651dd131b10198c5b031c9be8e453f3f
Italo Javosky <silva.camilo@escolar.ifrn.edu.br>
2026-06-23 11:32:59 -0300

    jfsdn
    sfd

--- The hope of life/Assets/Scripts/Tumbleweedspawn.cs
+++ The hope of life/Assets/Scripts/Tumbleweedspawn.cs
@@ -5,16 +5,10 @@ public class Tumbleweedspawn : MonoBehaviour
     public GameObject arbusto;
     public Transform Spawnpoint;
 
-    private bool spawned = false;
-
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (spawned) return;
-
         if (other.CompareTag("Player"))
         {
-            spawned = true;
-
             GameObject tumbleweed = Instantiate(
                 arbusto,
                 Spawnpoint.position,
